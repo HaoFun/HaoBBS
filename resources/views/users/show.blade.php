1ad9a29a0af49c80129d82db50ab7406f7134800
HaoChang <howhow926@gmail.com>
2018-01-30 16:30:12 +0800

    User profile page

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title',$user->name.'個人中心')
+
+@section('content')
+<div class="row">
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="media">
+                    <div class="text-center">
+                        <img class="thumbnail img-responsive" src="{{ asset('images/avatar/avatar.png') }}"
+                        alt="{{ $user->name.'頭像' }}" width="300px" height="300px">
+                    </div>
+                    <div class="media-body">
+                        <hr>
+                        <h4><strong>個人簡介</strong></h4>
+                        <p>Hello world!</p>
+                        <hr>
+                        <h4><strong>註冊於</strong></h4>
+                        <p>January 13 2018</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <span>
+                    <h1 class="panel-title pull-left" style="font-size: 30px;">{{ $user->name }}&nbsp;&nbsp;&nbsp;<small>{{ $user->email }}</small></small></h1>
+                </span>
+            </div>
+        </div>
+        <hr>
+
+        {{--用戶發布的內容--}}
+        <div class="panel panel-default">
+            <div class="panel-body">
+                暫無資料
+            </div>
+        </div>
+    </div>
+</div>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/','PagesController@root')->name('root');
 
 Auth::routes();
+
+/* User route */
+Route::resource('users','UsersController',['only' => ['show','update','edit']]);
+Route::get('/user/{user}','UsersController@show')->name('users.show');  //隱式綁定
+
+
